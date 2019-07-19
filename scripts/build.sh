18f66faee1e62ba700cf7ebb1c65ee8e4e86fbc4
Andrew Spinks <andrewspinks@gmail.com>
2019-07-19 21:18:43 +0900

    Try to get build to fail on test failure.

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,12 +14,7 @@ carthage build --no-skip-current --platform $CARTHAGE_PLATFORM
 echo "#### Testing DEBUG configuration for SwiftPM compatibility ####"
 swift build
 
-# Carthage - debug
+# Run tests
 echo "#### Testing DEBUG configuration for scheme: $SCHEME, with destination: $DESTINATION ####"
 echo "Running: \"xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty -c;\""
-xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty -c;
-
-# Carthage - release
-echo "#### Testing RELEASE configuration for scheme: $SCHEME, with destination: $DESTINATION ####"
-echo "Running: \"xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty -c;\""
-xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty -c;
+set -o pipefail && xcodebuild -project $PROJECT_NAME -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty -c;
